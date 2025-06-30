12c85a8ab68f4fe9332ab4d36deb74070d7ce950
Alexander Schwartz <alex@asd-tech.com>
2025-06-30 17:11:20 +0300

    Updating almost works - autorestart is not working

---
+++ VesperApp/release_pack-stable.ps1
@@ -0,0 +1,2 @@
+﻿dotnet publish -c Release -r win-x64 -o publish
+vpk pack --packId VesperApp --packVersion 1.0.26 --packDir publish --mainExe VesperApp.exe --channel win-x64-stable --icon Assets\bat.ico
