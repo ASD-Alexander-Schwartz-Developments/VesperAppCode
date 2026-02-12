d79cd54ab22c6e8670bd344cd7be37ff39a2ddc6
asdtech <alex@asd-tech.com>
2026-02-12 21:54:01 +0200

    project changes before going open source

--- VesperApp/release_pack-stable.ps1
+++
@@ -1,2 +0,0 @@
-﻿dotnet publish -c Release -r win-x64 -o publish
-vpk pack --packId VesperApp --packVersion 1.0.27 --packDir publish --mainExe VesperApp.exe --channel win-x64-stable --icon Assets\bat.ico
