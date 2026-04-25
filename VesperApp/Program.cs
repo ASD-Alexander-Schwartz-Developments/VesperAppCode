e794bab9e90cdfd091e5b3837825dac9d5b08c48
asdtech <alex@asd-tech.com>
2026-04-25 12:39:29 +0300

    added pack instructions

--- VesperApp/Program.cs
+++ VesperApp/Program.cs
@@ -63,3 +63,10 @@ namespace VesperApp
                 .UseReactiveUI();
     }
 }
+/*
+ * 
+ * Release:
+ * dotnet publish -c Release -r win-x64 -o publish
+ * vpk pack --packId VesperApp --packVersion 1.0.28 --packDir publish --mainExe VesperApp.exe --channel win-x64-stable --icon VesperApp\Assets\bat.ico
+ * 
+ * */
